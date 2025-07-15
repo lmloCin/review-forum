fcd9e87ee61b1872ee9d3a543457fe5d83bef349
lmloCin <lmlo@cin.ufpe.br>
2025-07-15 09:37:05 -0300

    feat: backend conteudo feature

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,11 +1,14 @@
 import forumRouter from "./ForumApi";
 import MovieRouter from "./MovieApi";
+import ReviewRouter from "./ReviewApi";
 import { Router } from "express";
 
 const endpointsRouter = Router()
 
 endpointsRouter.use('/movies', MovieRouter)
 endpointsRouter.use('/forums', forumRouter)
+endpointsRouter.use('/reviews', ReviewRouter)
+
 
 
 export default endpointsRouter
