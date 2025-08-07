0ede6ea98a3a36efa63fbf9a25591f6caca6509a
vncrodrigues13 <viniciusalmeidarodrigues96@gmail.com>
2025-08-07 00:50:45 -0300

    fix issues

--- front-end/.next/server/middleware-build-manifest.js
+++ front-end/.next/server/middleware-build-manifest.js
@@ -27,6 +27,21 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/src_pages_forum_Forum_64fc9743.css",
       "static/chunks/src_pages_forum_index_tsx_5771e187._.js",
       "static/chunks/src_pages_forum_index_tsx_ffa34f67._.js"
+    ],
+    "/forum/[forumId]": [
+      "static/chunks/node_modules_next_dist_compiled_next-devtools_index_82a36480.js",
+      "static/chunks/node_modules_next_dist_compiled_43e01ce8._.js",
+      "static/chunks/node_modules_next_dist_shared_lib_492fcef6._.js",
+      "static/chunks/node_modules_next_dist_client_becf32a6._.js",
+      "static/chunks/node_modules_next_dist_a3e9a08f._.js",
+      "static/chunks/node_modules_next_router_47d9bb1c.js",
+      "static/chunks/node_modules_react-dom_82bb97c6._.js",
+      "static/chunks/node_modules_axios_lib_9aa2336a._.js",
+      "static/chunks/node_modules_a80c7711._.js",
+      "static/chunks/[root-of-the-server]__2549ec38._.js",
+      "static/chunks/src_46a81706._.css",
+      "static/chunks/src_pages_forum_[forumId]_index_tsx_5771e187._.js",
+      "static/chunks/src_pages_forum_[forumId]_index_tsx_ac0365b0._.js"
     ]
   },
   "devFiles": [],
